8e6cee3547ec7ba22bd14b290cf72f24c16fa6bb
androxxe <andriopratama16@gmail.com>
2024-03-01 02:11:08 +0700

    feat: init babel-plugin-module-resolver

---
+++ src/components/form/input-text/index.tsx
@@ -0,0 +1,5 @@
+import { Text } from "react-native"
+
+export const InputText = () => {
+  return <Text>assd</Text>
+};
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,11 +2,13 @@ import * as React from 'react';
 import { View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { InputText } from '@/components/form';
 
 function HomeScreen() {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Home Screen</Text>
+      <InputText />
     </View>
   );
 }
