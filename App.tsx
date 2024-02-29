0426fe01d448f3001d44fc96e963280836e69d4c
androxxe <andriopratama16@gmail.com>
2024-03-01 01:52:33 +0700

    feat: init react-query

--- App.tsx
+++ App.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import { Routes } from './src/routes';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+export const queryClient = new QueryClient()
 
 function App(): React.JSX.Element {
   return (
-    <GestureHandlerRootView style={{ flex: 1 }}>
-      <Routes />
-    </GestureHandlerRootView>
+    <QueryClientProvider client={queryClient}>
+      <GestureHandlerRootView style={{ flex: 1 }}>
+        <Routes />
+      </GestureHandlerRootView>
+    </QueryClientProvider>
   );
 }
 
